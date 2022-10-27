af1eae0ab8e9656fa42ef687458fef1483d2f6b7
Jorge Dorio <jorgegabrielbd.jj@gmail.com>
2022-10-27 15:18:34 -0300

    4 - O programa deve transferir dinheiro entre contas (erro)

--- src/trybank.Test/TestFourthReq.cs
+++ src/trybank.Test/TestFourthReq.cs
@@ -8,10 +8,20 @@ namespace trybank.Test;
 public class TestFourthReq
 {
     [Theory(DisplayName = "Deve transefir um valor com uma conta logada")]
-    [InlineData(0, 0)]
+    [InlineData(50, 20)]
     public void TestTransferSucess(int balance, int value)
     {        
-        throw new NotImplementedException();
+			Trybank tb = new Trybank();
+
+			tb.RegisterAccount(11,11,11);
+			tb.RegisterAccount(22,22,22);
+			tb.Login(11,11,11);
+			tb.Bank[tb.loggedUser,3] = balance;
+
+			tb.Transfer(11,11, value);
+
+			tb.Bank[tb.loggedUser,3].Should().Be(balance - value);
+			tb.Bank[1,3].Should().Be(value);
     }
 
     [Theory(DisplayName = "Deve lançar uma exceção de usuário não logado")]
--- src/trybank/Trybank.cs
+++ src/trybank/Trybank.cs
@@ -2,94 +2,111 @@
 
 public class Trybank
 {
-    public bool Logged;
-    public int loggedUser;
-    
-    //0 -> Número da conta
-    //1 -> Agência
-    //2 -> Senha
-    //3 -> Saldo
-    public int[,] Bank;
-    public int registeredAccounts;
-    private int maxAccounts = 50;
-    public Trybank()
-    {
-        loggedUser = -99;
-        registeredAccounts = 0;
-        Logged = false;
-        Bank = new int[maxAccounts, 4];
-    }
+	public bool Logged;
+	public int loggedUser;
 
-    public void RegisterAccount(int number, int agency, int pass)
-    {
-			for(int i = 0; i < registeredAccounts; i++)
+	//0 -> Número da conta
+	//1 -> Agência
+	//2 -> Senha
+	//3 -> Saldo
+	public int[,] Bank;
+	public int registeredAccounts;
+	private int maxAccounts = 50;
+	public Trybank()
+	{
+		loggedUser = -99;
+		registeredAccounts = 0;
+		Logged = false;
+		Bank = new int[maxAccounts, 4];
+	}
+
+	public void RegisterAccount(int number, int agency, int pass)
+	{
+		for(int i = 0; i < registeredAccounts; i++)
+		{
+			if(Bank[i,0] == number && Bank[i,1] == agency)
 			{
-				if(Bank[i,0] == number && Bank[i,1] == agency)
-				{
-					throw new ArgumentException("A conta já está sendo usada!");
-				};
-			}
+				throw new ArgumentException("A conta já está sendo usada!");
+			};
+		}
 
-			Bank[registeredAccounts, 0] = number;
-			Bank[registeredAccounts, 1] = agency;
-			Bank[registeredAccounts, 2] = pass;
+		Bank[registeredAccounts, 0] = number;
+		Bank[registeredAccounts, 1] = agency;
+		Bank[registeredAccounts, 2] = pass;
 
-			registeredAccounts++;
-    }
+		registeredAccounts++;
+	}
 
-    public void Login(int number, int agency, int pass)
-    {
-			if(Logged) throw new AccessViolationException("Usuário já está logado");
+	public void Login(int number, int agency, int pass)
+	{
+		bool found = false;
+		if(Logged) throw new AccessViolationException("Usuário já está logado");
 
-			for(int i = 0; i < registeredAccounts; i++)
+		for(int i = 0; i < registeredAccounts; i++)
+		{
+			if(Bank[i,0] == number && Bank[i,1] == agency && Bank[i,2] == pass)
 			{
-				if(Bank[i,0] == number && Bank[i,1] == agency && Bank[i,2] == pass)
-				{
-					Logged = true;
-					loggedUser = i;
-				} else if (Bank[i,0] == number && Bank[i,1] == agency && Bank[i,2] != pass)
-				{
-					throw new ArgumentException("Senha incorreta");
-				} else
-				{
-					throw new ArgumentException("Agência + Conta não encontrada");
-				};
+				Logged = true;
+				loggedUser = i;
+				found = true;
+			} else if (Bank[i,0] == number && Bank[i,1] == agency && Bank[i,2] != pass)
+			{
+				throw new ArgumentException("Senha incorreta");
+			};
+		}
+
+		if(!found) throw new ArgumentException("Agência + Conta não encontrada");
+	}
+
+	public void Logout()
+	{
+		if(!Logged) throw new AccessViolationException("Usuário não está logado");
+
+		Logged = false;
+		loggedUser = -99;
+	}
+
+	public int CheckBalance()
+	{
+		if(!Logged) throw new AccessViolationException("Usuário já está logado");
+
+		return Bank[loggedUser,3];
+	}
+
+	public void Transfer(int destinationNumber, int destinationAgency, int value)
+	{
+		bool found = false;
+		if(!Logged) throw new AccessViolationException("Usuário já está logado");
+
+		if(value > Bank[loggedUser,3]) throw new InvalidOperationException("Saldo insuficiente");
+
+		for(int i = 0; i < registeredAccounts; i++)
+		{
+			if(Bank[i,0] == destinationNumber && Bank[i,1] == destinationAgency)
+			{
+				Bank[loggedUser, 3] -= value;
+				Bank[i, 3] += value;
+				found = true;
+				break;
 			}
-    }
+		}
 
-    public void Logout()
-    {
-			if(!Logged) throw new AccessViolationException("Usuário não está logado");
-			
-			Logged = false;
-			loggedUser = -99;
-    }
+		if(!found) throw new ArgumentException("Agência + Conta não encontrada");
+	}
 
-    public int CheckBalance()
-    {
-			if(!Logged) throw new AccessViolationException("Usuário já está logado");
-			
-			return Bank[loggedUser,3];
-    }
+	public void Deposit(int value)
+	{
+		if(!Logged) throw new AccessViolationException("Usuário já está logado");
 
-    public void Transfer(int destinationNumber, int destinationAgency, int value)
-    {
-        throw new NotImplementedException();
-    }
+		Bank[loggedUser,3] += value;
+	}
 
-    public void Deposit(int value)
-    {
-			if(!Logged) throw new AccessViolationException("Usuário já está logado");
-			
-			Bank[loggedUser,3] += value;
-    }
+	public void Withdraw(int value)
+	{
+		if(!Logged) throw new AccessViolationException("Usuário já está logado");
 
-    public void Withdraw(int value)
-    {
-			if(!Logged) throw new AccessViolationException("Usuário já está logado");
+		if(value > Bank[loggedUser,3]) throw new InvalidOperationException("Saldo insuficiente");
 
-			if(value > Bank[loggedUser,3]) throw new InvalidOperationException("Saldo insuficiente");
-
-			Bank[loggedUser,3] -= value;
-    }
+		Bank[loggedUser,3] -= value;
+	}
 }
